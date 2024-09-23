927146486062989269806bcb241174d0afa23722
antoni <antoni_pereblasco@ceroca.cat>
2024-09-23 20:24:29 +0200

    limits pantalla

--- Assets/Scripts/NauJugador.cs
+++ Assets/Scripts/NauJugador.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class NauJugador : MonoBehaviour
 {
     private float _vel;
+
+    Vector2 minPantalla, maxPantalla;
     // Start is called before the first frame update
     void Start()
     {
         _vel = 8f;
+        minPantalla = Camera.main.ViewportToWorldPoint( new Vector2(0 , 0));
+        maxPantalla = Camera.main.ViewportToWorldPoint(new Vector2 (1, 1));
+        
     }
 
     // Update is called once per frame
@@ -25,6 +30,9 @@ public class NauJugador : MonoBehaviour
         Vector2 novaPos = transform.position;// posiscio actual del objecte amb el transform.position
 
         novaPos = novaPos + direccioIndicada * _vel * Time.deltaTime ;
+
+        novaPos.x = Mathf.Clamp( novaPos.x , minPantalla.x , maxPantalla.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, minPantalla.y, maxPantalla.y);
         transform.position = novaPos;
 
        

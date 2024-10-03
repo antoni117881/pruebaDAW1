b32f50db0401fc804ed126a497ab4ed32cc137df
antoni <antoni_pereblasco@ceroca.cat>
2024-10-03 16:04:37 +0200

    generador numeros iniciat

---
+++ Assets/Scripts/GenearadorNmeros.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenearadorNmeros : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        invokeRepiting("GeneraNumero", 1f, 2f);
+    }
+
+    private void GeneraNumero()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Numero.cs
+++ Assets/Scripts/Numero.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Numero : MonoBehaviour
 {
+    private float vel;
+    Vector2 minPantalla;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        vel = 3f;
+        minPantalla = Camera.main.ViewportToWorldPoint(new Vector2(0,0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 posActual = transform.position;
+        posActual =posActual + new Vector2(0, -1) * vel * Time.deltaTime;
+        transform.position = posActual;
+        if(transform.position.y < minPantalla.y)
+        {
+            Destroy(gameObject);
+        }
     }
 }
